47af88eab63377894069a625e918c44750be0583
vlad <vladislav.brovikov@gmail.com>
2022-09-25 19:36:22 -0400

    make most fields optional

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -2,43 +2,43 @@ export interface Show {
   id: number;
   url: string;
   name: string;
-  type: string;
-  language: string;
-  genres: string[];
-  status: string;
-  runtime: number;
-  averageRuntime: number;
-  premiered: Date;
-  ended: Date;
-  officialSite: string;
-  schedule: Schedule;
-  rating: Rating;
-  weight: number;
-  network: Network;
-  webChannel: string;
-  dvdCountry: string;
-  externals: Externals;
-  image: ImageLinks;
-  summary: string;
-  updated: number;
-  _links: Links;
+  type?: string;
+  language?: string;
+  genres?: string[];
+  status?: string;
+  runtime?: number;
+  averageRuntime?: number;
+  premiered?: Date;
+  ended?: Date;
+  officialSite?: string;
+  schedule?: Schedule;
+  rating?: Rating;
+  weight?: number;
+  network?: Network;
+  webChannel?: string;
+  dvdCountry?: string;
+  externals?: Externals;
+  image?: ImageLinks;
+  summary?: string;
+  updated?: number;
+  _links?: Links;
 }
 
 export interface Episode {
   id: number;
   url: string;
   name: string;
-  season: number;
-  number: number;
-  type: string;
-  airdate: Date;
-  airtime: string;
-  airstamp: Date;
-  runtime: number;
-  rating: Rating;
-  image: ImageLinks;
-  summary: string;
-  _links: Links;
+  season?: number;
+  number?: number;
+  type?: string;
+  airdate?: Date;
+  airtime?: string;
+  airstamp?: Date;
+  runtime?: number;
+  rating?: Rating;
+  image?: ImageLinks;
+  summary?: string;
+  _links?: Links;
 }
 
 export interface Links {
@@ -48,38 +48,38 @@ export interface Links {
 }
 
 export interface Link {
-  href: string;
+  href?: string;
 }
 
 export interface Externals {
-  tvrage: number;
-  thetvdb: number;
-  imdb: string;
+  tvrage?: number;
+  thetvdb?: number;
+  imdb?: string;
 }
 
 export interface ImageLinks {
-  medium: string;
-  original: string;
+  medium?: string;
+  original?: string;
 }
 
 export interface Network {
-  id: number;
-  name: string;
-  country: Country;
-  officialSite: string;
+  id?: number;
+  name?: string;
+  country?: Country;
+  officialSite?: string;
 }
 
 export interface Country {
-  name: string;
-  code: string;
-  timezone: string;
+  name?: string;
+  code?: string;
+  timezone?: string;
 }
 
 export interface Rating {
-  average: number;
+  average?: number;
 }
 
 export interface Schedule {
-  time: string;
-  days: string[];
+  time?: string;
+  days?: string[];
 }
