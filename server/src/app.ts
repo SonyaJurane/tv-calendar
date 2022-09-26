7a3de53ce38208b0794bca067fce66ff3f3a7732
vlad <vladislav.brovikov@gmail.com>
2022-09-26 13:05:03 -0400

    move schema to a sep file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,81 +1,11 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
 import Axios from "axios";
 import { Show } from "./types";
+import Schema from "./graphqlSchema";
 
 const app = express();
 const port = 8080;
-
-const schema = buildSchema(`
-  type Rating {
-    average: Float
-  }
-
-  type Country {
-    name: String
-    code: String
-    timezone: String
-  }
-
-  type Schedule {
-    time: String
-    days: [String]
-  }
-
-  type Network {
-    id: Int
-    name: String
-    country: Country
-    officialSite: String
-  }
-
-  type ExternalIds {
-    tvrage: Int
-    thetvdb: Int
-    imdb: String
-  }
-
-  type Links {
-    self: String
-    previousepisode: String
-    nextepisode: String
-  }
-
-  type ImageLinks {
-    medium: String
-    original: String
-  }
-
-  type Show {
-    id: Int!
-    name: String
-    type: String
-    language: String
-    genres: [String]
-    status: String
-    runtime: Int
-    averageRuntime: Int
-    premiered: String
-    ended: String
-    officialSite: String
-    schedule: Schedule
-    rating: Rating
-    weight: Int
-    network: Network
-    webChannel: String
-    dvdCountry: String
-    externals: ExternalIds
-    image: ImageLinks
-    summary: String
-    updated: Int
-    _links: Links
-  }
-
-  type Query {
-    search(prompt : String!): [Show]
-  }
-`);
 
 const searchShow = async (namePrompt: string): Promise<[Show] | undefined> => {
   try {
@@ -84,7 +14,6 @@ const searchShow = async (namePrompt: string): Promise<[Show] | undefined> => {
     const response = await Axios.get(requestString);
     const data = response.data;
     const shows = data.map(({ show }: any) => ({ ...show }));
-    console.log(shows);
     return shows;
   } catch (error) {
     console.error(error);
@@ -104,7 +33,7 @@ const root = {
 
 app.use("/graphql", (req, res, next) => {
   const promise = graphqlHTTP({
-    schema,
+    schema: Schema,
     rootValue: root,
     graphiql: true,
   })(req, res);
---
+++ server/src/graphqlSchema.ts
@@ -0,0 +1,108 @@
+import { buildSchema } from "graphql";
+
+const ratingSchema = `  
+    type Rating {
+        average: Float
+    }
+`;
+
+const countrySchema = `  
+    type Country {
+        name: String
+        code: String
+        timezone: String
+    }
+`;
+
+const scheduleSchema = `  
+    type Schedule {
+        time: String
+        days: [String]
+    }
+`;
+
+const networkSchema = `  
+    type Network {
+        id: Int
+        name: String
+        country: Country
+        officialSite: String
+    }
+`;
+
+const externalIdsSchema = `  
+    type ExternalIds {
+        tvrage: Int
+        thetvdb: Int
+        imdb: String
+    }
+`;
+
+const linkSchema = `
+    type Link {
+        href: String
+    }
+`;
+
+const linksSchema = `  
+type Links {
+    self: Link
+    previousepisode: Link
+    nextepisode: Link
+  }
+`;
+
+const imageLinksSchema = `  
+    type ImageLinks {
+        medium: String
+        original: String
+    }
+`;
+
+const showSchema = `  
+    type Show {
+        id: Int!
+        name: String
+        type: String
+        language: String
+        genres: [String]
+        status: String
+        runtime: Int
+        averageRuntime: Int
+        premiered: String
+        ended: String
+        officialSite: String
+        schedule: Schedule
+        rating: Rating
+        weight: Int
+        network: Network
+        webChannel: String
+        dvdCountry: String
+        externals: ExternalIds
+        image: ImageLinks
+        summary: String
+        updated: Int
+        _links: Links
+    }
+`;
+
+const querySchema = `  
+    type Query {
+        search(prompt : String!): [Show]
+    }
+  `;
+
+export const Schema = buildSchema(`
+  ${ratingSchema}
+  ${countrySchema}
+  ${scheduleSchema}
+  ${networkSchema}
+  ${externalIdsSchema}
+  ${linkSchema}
+  ${linksSchema}
+  ${imageLinksSchema}
+  ${showSchema}
+  ${querySchema}
+`);
+
+export default Schema;
