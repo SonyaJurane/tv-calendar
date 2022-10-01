6c24949949cc3c4708d10fddc5f517a5d60b4fbb
SonyaJurane <sonyajurane@gmail.com>
2022-10-01 13:08:14 -0400

    add-landing-page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD:src/App.tsx
-import { Main, About, Landing } from "./pages";
+import { Main, About, Landing, Search } from "./pages";
 import { createTheme, ThemeProvider } from "@mui/material";
 // import Paper from '@mui/material/Paper';
 // import Image from "./background.png";
 
-const theme = createTheme  ({
+const theme = createTheme({
   typography: {
-    fontFamily: 'Open Sans',
+    fontFamily: "Open Sans",
     fontWeightLight: 400,
     fontWeightRegular: 500,
     fontWeightBold: 600,
     h1: {
-      fontFamily: 'Sora',
+      fontFamily: "Sora",
       fontSize: 92,
     },
     h2: {
@@ -21,33 +20,32 @@ const theme = createTheme  ({
       lineHeight: 1.5,
     },
     button: {
-      fontFamily: 'Sora',
+      fontFamily: "Sora",
       fontSize: 32,
-      textTransform: 'lowercase',
-      letterSpacing: '0.08em',
-    }
+      textTransform: "lowercase",
+      letterSpacing: "0.08em",
+    },
   },
 
   components: {
     MuiButton: {
       styleOverrides: {
         root: {
-          size: '500',
-          border: '2px solid #000000',
-          borderRadius: '55.5px',
-          width: '395px',
-          height: '111px',
-          color: '#000000',
+          size: "500",
+          border: "2px solid #000000",
+          borderRadius: "55.5px",
+          width: "395px",
+          height: "111px",
+          color: "#000000",
           "&:hover": {
-            color: '#ffffff',
-            backgroundColor: 'black',
+            color: "#ffffff",
+            backgroundColor: "black",
           },
         },
       },
     },
   },
 });
-
 
 export const App = (): JSX.Element => {
   return (
@@ -56,20 +54,9 @@ export const App = (): JSX.Element => {
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
         <Route path="/landing" element={<Landing />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </ThemeProvider>
-=======
-import { Main, About, Landing, Search } from "./pages";
-
-export const App = (): JSX.Element => {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="/search" element={<Search />} />
-    </Routes>
->>>>>>> master:client/src/App.tsx
   );
 };
 
